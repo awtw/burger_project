ce12543b7cb6af4e08994279a102c18ad0604b9a
awtw <alec.wang.tpe@gmail.com>
2019-11-28 23:29:18 +0800

    Updates

---
+++ precache-manifest.752d85bd8b8e1fe6b2d91c4249c22689.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "065ba040e438c517cefa8fce7584de7d",
+    "url": "/burger_project/index.html"
+  },
+  {
+    "revision": "3beb4ab0d5712e8fa51f",
+    "url": "/burger_project/static/css/main.d72d8498.chunk.css"
+  },
+  {
+    "revision": "9fe90d58d1a2d1318ce1",
+    "url": "/burger_project/static/js/2.52c35f30.chunk.js"
+  },
+  {
+    "revision": "3beb4ab0d5712e8fa51f",
+    "url": "/burger_project/static/js/main.14646d86.chunk.js"
+  },
+  {
+    "revision": "786bc41bf60ebcc031bf",
+    "url": "/burger_project/static/js/runtime-main.5505888a.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger_project/static/media/burger-logo.b8503d26.png"
+  }
+]);
